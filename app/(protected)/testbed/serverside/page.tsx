ae35d7e057a248c59cc55ac46e3201e1da435cce
judaihyun <judaihyun1@gmail.com>
2026-05-09 12:12:02 +0900

    push

--- app/(protected)/testbed/serverside/page.tsx
+++ app/(protected)/testbed/serverside/page.tsx
@@ -2,9 +2,10 @@
 import AG_GRID_KEY from "@/constants/key";
 import { AllEnterpriseModule, ColDef } from "ag-grid-enterprise";
 import { AgGridProvider, AgGridReact } from "ag-grid-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Page() {
+    const gridRef = useRef<AgGridReact>(null);
     const [rowData, setRowData] = useState([
         { make: "Tesla", model: "Model Y", price: 64950, electric: true },
         { make: "Ford", model: "F-Series", price: 33850, electric: false },
@@ -23,10 +24,22 @@ export default function Page() {
             <h1 className="text-2xl font-bold mb-4">서버 사이드 렌더링 테스트</h1>
             <p className="mb-2">이 페이지는 서버 사이드에서 렌더링됩니다.</p>
             <p>데이터 그리드 테스트를 위해 CellRenderer 컴포넌트를 포함할 수 있습니다.</p>
-
+            <button
+                className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+                onClick={() => {
+                    const gridApi = gridRef.current?.api;
+                    if (gridApi) {
+                        gridApi.applyColumnState({
+                            state: [{ colId: "price", sort: "asc" }],
+                        });
+                    }
+                }}
+            >
+                update cell state
+            </button>
             <AgGridProvider modules={[AllEnterpriseModule]} licenseKey={AG_GRID_KEY}>
                 <div className="flex-1 w-full">
-                    <AgGridReact rowData={rowData} columnDefs={colDefs} />
+                    <AgGridReact ref={gridRef} rowData={rowData} columnDefs={colDefs} />
                 </div>
             </AgGridProvider>
         </div>
